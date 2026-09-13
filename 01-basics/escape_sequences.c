4654ad0bf489bff148485570e528806b8ca99940
Manansh Chourey <mananshchourey.mts@gmail.com>
2026-09-13 16:54:20 +0530

    Complete C basics

---
+++ 01-basics/escape_sequences.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("New line: Hello \nWorld \n");
+    printf("Tab: A \tB\tC \n");
+    printf("Backslash: \\ \n");
+    printf("Quote: \"Hello\" \n");
+    printf("Single quote: \'A\' \n");
+    printf("Alert: \a \n");
+    printf("Backspace: A\b B \n");
+    printf("Carriage return: Hello\r World\n");
+    printf("Form feed: A \fB \n");
+    printf("Vertical tab: A\vB \n");
+    printf("Question mark: ? \n");
+
+    return 0;
+}
